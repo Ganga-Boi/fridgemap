971aca5d418c81054ad91fa4f14a310a2b08b20c
GangaBoi <132343514+Ganga-Boi@users.noreply.github.com>
2025-12-30 23:26:06 +0100

    analyze.edge.js

--- api/analyze.edge.js
+++ api/analyze.edge.js
@@ -1,1 +1,15 @@
+export const runtime = "edge";
+
+export default function handler() {
+  return new Response(
+    JSON.stringify({ ok: true, message: "EDGE API OK" }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      }
+    }
+  );
+}
 
