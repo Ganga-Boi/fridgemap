c749b91d7d7903b0db8baa72d2357cb5ad7c5838
GangaBoi <132343514+Ganga-Boi@users.noreply.github.com>
2025-12-30 14:52:42 +0100

    analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,3 +1,11 @@
 export default async function handler(req, res) {
-  res.status(200).json({ status: "API OK" });
+  if (req.method === "GET") {
+    return res.status(200).json({ status: "API OK" });
+  }
+
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+
+  return res.status(200).json({ status: "POST OK" });
 }
